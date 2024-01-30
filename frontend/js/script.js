5690b9112aabecb04cf74acfbcb32b5e065152d5
Votka69 <124756766+Votka69@users.noreply.github.com>
2024-01-30 18:19:32 -0300

    script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:5500")
+    websocket = new WebSocket("wss://chat-nexus.onrender.com")
     websocket.onmessage = processMessage
 }
 
